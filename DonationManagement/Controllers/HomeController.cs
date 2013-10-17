bf6dbb177bf3798402aece03121d0725f5c31684
adeelm <adeelm7@gmail.com>
2013-10-17 17:21:50 -0500

    DMS

---
+++ DonationManagement/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DonationManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Index - Dashboard";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "About DMS";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "DMS Contact Page";
+
+            return View();
+        }
+    }
+}
